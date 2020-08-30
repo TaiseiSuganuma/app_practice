032fc3c63bdcad31157c2604ac628a8e9040473e
TaiseiSuganuma <gasu.tako4@gmail.com>
2020-08-31 02:57:55 +0900

    tsとstorybookの導入

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: ["../{pages,components}/**/*.stor{ies,y}.{t,j}sx"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", { flow: false, typescript: true }]],
+      },
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
+};
